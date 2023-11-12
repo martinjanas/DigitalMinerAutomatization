8d700feb66570a24d68153da44c99791ba02110d
MartiN <44605473+martinjanas@users.noreply.github.com>
2023-11-12 20:27:41 +0100

    Add files via upload

---
+++ utils.lua
@@ -0,0 +1,22 @@
+utils = 
+{
+    function is_chunky_turtle_fn()
+        local list = peripheral.getNames()
+    
+        for _, side in pairs(list) do
+            if side == nil then
+                continue()
+            end
+    
+            local type = peripheral.getType(side)
+    
+            if not string.find(type, "chunky") then
+                return false
+            end
+    
+            return true
+        end
+    
+        return false
+    end
+}
